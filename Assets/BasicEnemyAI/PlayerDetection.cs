c04c016cb66c9a99db4cdcbb27400ee15c06affb
Russell Li <russell.li@rocketmail.com>
2023-10-20 15:01:25 -0400

    Trying to add LOS

--- Assets/BasicEnemyAI/PlayerDetection.cs
+++ Assets/BasicEnemyAI/PlayerDetection.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using static UnityEditor.PlayerSettings;
 
 public class PlayerDetection : MonoBehaviour
 {
     [Header("Sensing Suite")]
-    public bool LOS;
+    public bool hasLOS;
     private EnemyBehaviour enemyBehaviour;
+    public LayerMask collisionLayerMask;
 
     // Start is called before the first frame update
     void Start()
     {
-        enemyBehaviour = this.transform.parent.GetComponent<EnemyBehaviour>();
-        LOS = false;
+        enemyBehaviour = transform.parent.GetComponent<EnemyBehaviour>();
+        hasLOS = false;
         gameObject.GetComponent<SphereCollider>().enabled = true;
     }
 
