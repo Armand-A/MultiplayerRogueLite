b1e3385fb08ad988cbf8c63c24ce2dfada13e460
Russell Li <russell.li@rocketmail.com>
2023-10-20 13:52:02 -0400

    Added Enemy Attacking

--- Assets/BasicEnemyAI/EnemyBehaviour.cs
+++ Assets/BasicEnemyAI/EnemyBehaviour.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour
 {
+    private bool onCooldown;
     // Start is called before the first frame update
     void Start()
     {
-        
+        onCooldown = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            if (!onCooldown)
+            {
+                onCooldown = true;
+                Debug.Log("Attacking");
+                StartCoroutine(Cooldown());
+            }
+        }
+    }
+    IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(1);
+        onCooldown = false;
     }
 }
