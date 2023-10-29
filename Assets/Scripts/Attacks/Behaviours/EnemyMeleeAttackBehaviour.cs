7cbb9768eb38f385ccb9ad30b210b0f09b1f42e7
Russell Li <russell.li@rocketmail.com>
2023-10-29 11:53:15 -0400

    Added some attacks to the enemies

---
+++ Assets/Scripts/Attacks/Behaviours/EnemyMeleeAttackBehaviour.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMeleeAttackBehaviour : AttackBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Despawn());
+        transform.LookAt(_dstPos);
+        transform.position = _srcPos + 4 / 2 * transform.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Smacked Player");
+        }
+    }
+
+    IEnumerator Despawn()
+    {
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
+    }
+}
